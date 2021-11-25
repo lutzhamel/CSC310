10da805dda1ebbe009a4e5b00b088b5654b1e509
Lutz Hamel <lutz@MacBook-Pro.home>
2021-11-25 08:52:42 -0500

    deleted assets folder - it is now a separate repo

--- notes/assets/helloagain.py
+++
@@ -1,9 +0,0 @@
-"""
-helloagain.py
-
-Here we demonstrate that Python silently clobbers names clashes if you are not careful.
-"""
-
-def hello():
-    "Print out 'hello again!' and that's it"
-    print("hello again!")
--- notes/assets/helloworld.py
+++
@@ -1,9 +0,0 @@
-"""                                                                                                               
-helloworld.py
-This is the classic program every programmer writes when he or she learns
-a new programming language.
-"""
-
-def hello():
-    "Just print 'hello world!' and that's it"
-    print("hello world!") # print inserts a newline char  
